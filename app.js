2cb9025c27ce7f2c521a8f9ff8075881ca2af595
Javiera <sereno.javiera@gmail.com>
2018-05-18 17:36:16 -0400

    Botones Yes Not

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
 function nameSend() {
     var nameResult = document.getElementById("nameInput").value;
    document.getElementById("nameLabel").innerText = "Bienvenido/a " + nameResult;
-   document.getElementById("nameInput").style.visibility = "hidden";
-   document.getElementById("nameButton").style.visibility = "hidden";
+   document.getElementById("nameInput").style.display = "none";
+   document.getElementById("nameButton").style.display = "none";
 }
-
-    var wantToPlay = prompt("¿quieres jugar? s/n");
-    if (wantToPlay.toLowerCase() == "s") {
-        var answer1 = prompt("¿han egresado hombres en Laboratoria? s/n");
-        if (answer1.toLowerCase() == "n") {
-            document.getElementById("rightAnswersText").innerHTML =
-                document.getElementById("rightAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No han egresado hombres</p>"
-            "</div>";
-        } else {
-            document.getElementById("wrongAnswersText").innerHTML =
-                document.getElementById("wrongAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No han egresado hombres</p>"
-            "</div>";
-        }
-
-        var answer2 = prompt("¿hay laboratoria en concepción? s/n");
-        if (answer2.toLowerCase() == "n") {
-            document.getElementById("rightAnswersText").innerHTML =
-                document.getElementById("rightAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No hay laboratoria en concepción</p>"
-            "</div>";
-        } else {
-            document.getElementById("wrongAnswersText").innerHTML =
-                document.getElementById("wrongAnswersText").innerHTML +
-                "<div class='answer'>" +
-                "<p>No hay laboratoria en concepción</p>"
-            "</div>";
-        }
-    } else {
-        document.getElementById("warningMessage").innerText = "Bueno Chao";
+function functionNot() {
+    document.getElementById("messageNot").innerHTML = "Nos vemos para la próxima";
+    document.getElementById("wannaPlay").style.display = "none";
+}
+function functionYes() {
+    document.getElementById("questions1").style.display = "block";
 }
